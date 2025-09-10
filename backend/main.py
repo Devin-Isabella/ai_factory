ef18344cdb68732f71df9da7a8737824b1e24266
Devin-Isabella <66123492+Devin-Isabella@users.noreply.github.com>
2025-09-10 08:47:06 -0700

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+# Serve static files (CSS, JS, etc.)
+app.mount("/static", StaticFiles(directory="backend/app/static"), name="static")
+
+# Serve landing page
+@app.get("/")
+async def root():
+    return FileResponse("backend/app/static/index.html")
